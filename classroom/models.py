3fbdaf1035f27e46c37ebe5487535bd181521cb9
mfarzamalam <mfarzamalam@gmail.com>
2022-07-07 16:49:14 +0500

    added new tests for model and setup changes in models

--- classroom/models.py
+++ classroom/models.py
@@ -1,15 +1,16 @@
+from re import T
 from django.db import models
 
 # Create your models here.
 class Student(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
-    admission_number = models.IntegerField()
+    admission_number = models.IntegerField(null=True, blank=True)
     is_qualified = models.BooleanField(default=False)
     score = models.IntegerField()
 
     def __str__(self):
-        return f"{self.is_qualified}"
+        return f"{self.first_name}, {self.last_name}"
 
     def get_score_grade(self):
         if self.score < 30:
@@ -18,6 +19,8 @@ class Student(models.Model):
             return "Passed"
         elif self.score > 60:
             return "Pormoted"
+        elif self.score > 100 or self.score < 0:
+            return "Score should be in the range of 0-100"
 
     class Meta:
         db_table = u"student"
--- classroom/tests.py
+++ classroom/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from unittest import TestCase
+from classroom.models import Student
 
-# Create your tests here.
+
+class TestStudentModel(TestCase):
+    def setUp(self):
+        self.student_obj = Student.objects.create(first_name='farzam', last_name='alam',
+        is_qualified=True, score=200)
+    
+    def test_a_plus_b_is_equal_to_c(self):
+        print("\ntest_a_plus_b_is_equal_to_c")
+        a = 10
+        b = 10
+        c = 20
+        self.assertEqual(a+b, c)
+
+    def test_first_name_comma_last_name(self):
+        print("\ntest_first_name_comma_last_name")
+        expected_name = "%s, %s" %("farzam", "alam")
+        self.assertEqual(str(self.student_obj), expected_name)
+
+    def test_admission_number_is_greater_than_5_digit(self):
+        print("\ntest_admission_number_is_greater_than_5_digit")
+        self.student_obj.admission_number = 2000
+        self.student_obj.save()
+        self.assertEquals(len(str(self.student_obj.admission_number)), 5)
+
+    def tearDown(self):
+        Student.objects.all().delete()
